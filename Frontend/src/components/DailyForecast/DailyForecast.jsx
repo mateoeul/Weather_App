b16f16d2f7e46bb26864de05d10b4a80868601e1
Android Studio <devandroid@ort>
2025-08-22 10:38:13 -0300

    estructura completa

---
+++ Frontend/src/components/DailyForecast/DailyForecast.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+function DailyForecast({ forecastData }) {
+  return (
+    <div className="daily-forecast">{/* Render daily forecast data */}</div>
+  );
+}
+
+export default DailyForecast;
